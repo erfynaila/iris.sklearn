9ee9dec366ff123f72b9792800ea2aabace2d8dd
abbiyanaila <abbiyanaila@gmail.com>
2019-03-13 22:33:51 +0800

    fix: prediction iris with argparse

--- train.py
+++ train.py
@@ -9,53 +9,51 @@ import datetime
 from sklearn.model_selection import cross_val_predict
 
 parser = argparse.ArgumentParser(description="Training Iris")
-parser.add_argument('path', help='load file with format csv')
-parser.add_argument('features_X', type=str, help='features of iris')
-parser.add_argument('target', type=int, help='target of iris')
-parser.add_argument('X', type=str, help='featuter')
-parser.add_argument('y', type=int, help='target')
-parser.add_argument('train_size', type=float, help='break into two parameter X and y')
-parser.add_argument('random_state', type=int, help='break into two parameter X and y')
-parser.add_argument('standard_scaler', help='standardize features by removing the mean&scaling')
-parser.add_argument('fit_transform', help='fit to data, then transform it')
-parser.add_argument('training', help='process of training')
-parser.add_argument('cross_validation', help='predict the training of target')
-parser.add_argument('confusion_matrix', help='confusion matrix from predic')
-parser.add_argument('score_accuration', help='score accuration of training')
-parser.add_argument('datetime', help='for knowing the time if the file will to save')
-parser.add_argument('save_file', help='save file .pkl')
-parser.add_argument('load_file', help='load file .pkl')
+parser.add_argument('--path', type=str, help='load file with format csv')
+parser.add_argument('--feature_column', type=str, help='feature from iris dataset')
+parser.add_argument('--target_column', type=str, help='target from iris dataset')
+parser.add_argument('--train_size', default=0.9, type=float, help='give train size')
+parser.add_argument('--random_state', default=0, type=int, help='give random state')
+parser.add_argument('--algorithm', type=str, help='you can choose algorithm that you want to use')
+parser.add_argument('--save_to', default=None, help='you file will save in folder weight')
+args = parser.parse_args()
 
-df = pd.read_csv("dataset/iris.data")
-
-col = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+df = pd.read_csv(args.path)
+col = args.feature_column.split(',')
 X = df.loc[:, col]
 
 species_to_num = {'Iris-setosa': 0,
                   'Iris-versicolor': 1,
                   'Iris-virginica': 2}
-df['tmp'] = df['species'].map(species_to_num)
+df['tmp'] = df[args.target_column].map(species_to_num)
 y = df['tmp']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.9, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    train_size=args.train_size,
+                                                    random_state=args.random_state)
+
 sc_x = StandardScaler()
 X_std_train = sc_x.fit_transform(X_train)
 
-clf = svm.SVC(kernel='linear', C=1.0, verbose=True)
-clf.fit(X_std_train, y_train)
+if args.algorithm == 'svm':
+    clf = svm.SVC(kernel='linear', C=1.0, verbose=True)
+    clf.fit(X_std_train, y_train)
+elif args.algorithm == 'polinominal':
+    pass
 
 y_train_pred = cross_val_predict(clf, X_std_train, y_train, cv=3)
+cont_mat = confusion_matrix(y_train, y_train_pred)
+score = clf.score(X_std_train, y_train)
 
-confusion_matrix(y_train, y_train_pred)
+if args.save_to == None:
+    dstr = datetime.datetime.now().strftime("%y%m%d%H%M%S")
+    pkl_filename = "weight/test.sklearn_" + dstr + ".pkl"
+    with open(pkl_filename, 'wb') as file:
+        pickle.dump(clf, file)
+else:
+    pkl_filename = args.save_to
+    with open(pkl_filename, 'wb') as file:
+        pickle.dump(clf, file)
 
-clf.score(X_std_train, y_train)
-
-dstr= datetime.datetime.now().strftime("%y%m%d%H%M%S")
-pkl_filename = "iris.sklearn_"+dstr+".pkl"
-with open(pkl_filename, 'wb') as file:
-    pickle.dump(clf, file)
-
-with open(pkl_filename, 'rb') as file:
-    pickle_model = pickle.load(file)
 
 
